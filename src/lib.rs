bfa597d760f884c906cd65fd5a5a5ad19a26fab7
Cyanide-03 <aditya.ag0301@gmail.com>
2025-03-12 17:39:30 +0000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn calculate(num1 : f64, num2:f64, op:&str) -> String {
+    let result=match op {
+        "+" => num1 + num2,
+        "-" => num1 - num2,
+        "*" => num1 * num2,
+        "/" => {
+            if num2==0.0 {
+                return "Division by zero".to_string();
+            }
+            num1 / num2
+        },
+        _ => return "Invalid operator".to_string(),
+    };
+    format!("Result: {:.2}", result)
+}
